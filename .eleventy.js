fd8e06436116d6ff240c2c859996baa6893bbd62
Katie Lin <katielin019@gmail.com>
2025-06-25 11:04:27 -0700

    style experiment

--- .eleventy.js
+++ .eleventy.js
@@ -1,48 +1,33 @@
-// import fsp from 'fs/promises';
+// 
 import fs from 'fs';
 import path from 'path';
 import postcss from 'postcss';
 import tailwindcss from '@tailwindcss/postcss';
 
 export default async function (config) {
-    // clean _site directory before generating output
-    // config.on('eleventy.before', async ({ dir }) => {
-    //     await fsp.rm(dir.output, { recursive: true, force: true });
-    // });
-
     config.on('eleventy.before', async () => {
         const tailwindInputPath = path.resolve('./src/css/main.css');
         const tailwindOutputPath = './_site/css/main.css';
-        // const cssContent = fs.readFileSync(tailwindInputPath, 'utf8');
+        const cssContent = fs.readFileSync(tailwindInputPath, 'utf8');
 
         const outputDir = path.dirname(tailwindOutputPath);
         if (!fs.existsSync(outputDir)) {
             fs.mkdirSync(outputDir, { recursive: true });
         }
 
-        fs.readFile(tailwindInputPath, (err, css) => {
-            postcss([
-                tailwindcss(),
-            ])
-            .process(css, { from: tailwindInputPath, to: tailwindOutputPath })
-            .then(result => {
-                fs.writeFile(tailwindOutputPath, result.css, () => true)
-            });
+        const result = await processor.process(cssContent, {
+            from: tailwindInputPath,
+            to: tailwindOutputPath,
         });
 
-        // const result = await processor.process(cssContent, {
-        //     from: tailwindInputPath,
-        //     to: tailwindOutputPath,
-        // });
-
-        // fs.writeFileSync(tailwindOutputPath, result.css);
+        fs.writeFileSync(tailwindOutputPath, result.css);
         // https://robmc.dev/blog/add-css-to-your-eleventy-site/
         config.addWatchTarget('./src/css/main.css');
     });
 
-    // const processor = postcss([
-    //     tailwindcss(),
-    // ]);
+    const processor = postcss([
+        tailwindcss(),
+    ]);
 
     config.addPassthroughCopy('./src/assets/');
     config.addCollection('blogPosts', function (collectionAPI) {
