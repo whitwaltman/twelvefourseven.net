7bb52f3f051e1fe595af0ea5ffb33e9d8acc8449
Whit Waltman <whitwaltman@mailbox.org>
2025-07-01 13:42:20 -0700

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,18 +4,26 @@ set -eu     # fail on error and undefined variables
 SITE_DIR="_site"
 REMOTE="me@twelvefourseven.net"
 TARGET="/var/www/twelvefourseven.net"
-RSYNC_OPTS="-avhz"
-# RSYNC_OPTS="-avhz --delete"
+
+# Base rsync options
+RSYNC_OPTS=(-avhz)
+
+# Check for "--delete" flag
+if [[ "${1:-}" = "--delete" ]]; then
+    RSYNC_OPTS+=("--delete")
+    echo "⚠️  WARNING: --delete is enabled!"
+fi
 
 echo "Dry run first..."
-rsync $RSYNC_OPTS --dry-run "$SITE_DIR/" "$REMOTE:$TARGET"
+rsync ${RSYNC_OPTS[@]} --dry-run "$SITE_DIR/" "$REMOTE:$TARGET"
 
 echo
 echo "Does the dry run look okay? [y/N] "
 read CONFIRM
+
 if [ "$CONFIRM" = "y" ] || [ "$CONFIRM" = "Y" ]; then
     echo "Deploying..."
-    rsync $RSYNC_OPTS "$SITE_DIR/" "$REMOTE:$TARGET"
+    rsync ${RSYNC_OPTS[@]} "$SITE_DIR/" "$REMOTE:$TARGET"
     echo "Done!"
 else
     echo "Aborted."
