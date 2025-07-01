ded1e201ca16bf0059fee6e490d3b90fc2de0f58
Katie Lin <katielin019@gmail.com>
2025-06-30 17:22:02 -0700

    idk trying to add structure to this chaos

--- src/_data/site.cjs
+++ src/_data/site.cjs
@@ -2,6 +2,7 @@ module.exports = {
     title: "twelvefourseven.net",
     url: "https://twelvefourseven.net",
     language: "en",
-    description: "Katie Lin is a software designer and engineer. This is their weblog.",
+    description: "Katie Lin is a software designer and engineer. This is their internet home.",
     buildTime: new Date(),
+    explore: ["countdown", "startpage", "about", "contact"]
 }
