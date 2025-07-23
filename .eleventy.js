0fcc2d34e124efd45050bdfe3f492c55969013bc
Whit Waltman <whitwaltman@mailbox.org>
2025-07-23 12:31:05 -0700

    remove tailwind

--- .eleventy.js
+++ .eleventy.js
@@ -1,37 +1,12 @@
-import fs from 'fs';
-import path from 'path';
-import postcss from 'postcss';
-import tailwindcss from '@tailwindcss/postcss';
 import { eleventyImageTransformPlugin } from '@11ty/eleventy-img';
 
 export default async function (config) {
     config.addPlugin(eleventyImageTransformPlugin);
     
-    config.addPassthroughCopy('./src/assets/');
-
-    config.on('eleventy.before', async () => {
-        const tailwindInputPath = path.resolve('./src/css/main.css');
-        const tailwindOutputPath = './_site/css/main.css';
-        const cssContent = fs.readFileSync(tailwindInputPath, 'utf8');
-
-        const outputDir = path.dirname(tailwindOutputPath);
-        if (!fs.existsSync(outputDir)) {
-            fs.mkdirSync(outputDir, { recursive: true });
-        }
-
-        const result = await processor.process(cssContent, {
-            from: tailwindInputPath,
-            to: tailwindOutputPath,
-        });
-
-        fs.writeFileSync(tailwindOutputPath, result.css);
-        // https://robmc.dev/blog/add-css-to-your-eleventy-site/
-        config.addWatchTarget('./src/css/main.css');
+    config.addPassthroughCopy({
+        './src/assets/': 'assets',
+        './src/css/': 'css'
     });
-
-    const processor = postcss([
-        tailwindcss(),
-    ]);
 
     config.setFrontMatterParsingOptions({
         excerpt: true,
