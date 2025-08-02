660bb23298f3aa7bc2efd9a9126bf382625840e8
Whit Waltman <whitwaltman@mailbox.org>
2025-08-01 19:59:57 -0700

    remove section from computed data attributes (index not working)

--- src/src.11tydata.js
+++ src/src.11tydata.js
@@ -2,6 +2,5 @@ export default {
     layout: "layouts/page.njk",
     eleventyComputed: {
         permalink: (data) => data.page.filePathStem.replace("/src/", "") + "/index.html",
-        section: (data) => data.page.fileSlug,
     }
 }
