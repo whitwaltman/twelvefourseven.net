90792cdce74e04cd5e330f9a8225de682c6d83dd
Whit Waltman <whitwaltman@mailbox.org>
2025-09-23 19:40:09 -0700

    re-adding skeleton, base content, styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,35 @@
+import fs from 'fs';
+import path from 'path';
+import postcss from 'postcss';
+import tailwindcss from '@tailwindcss/postcss';
+
 export default async function (config) {
+    config.on('eleventy.before', async () => {
+        const inputPath = path.resolve('./src/css/main.css');
+        const outputPath = path.resolve('./_site/css/main.css');
+        const cssContent = fs.readFileSync(inputPath, 'utf8');
+
+        const outputDir = path.dirname(outputPath);
+        if (!fs.existsSync(outputDir)) {
+            fs.mkdirSync(outputDir, { recursive: true });
+        }
+
+        const result = await processor.process(cssContent, {
+            from: inputPath,
+            to: outputPath,
+        });
+        fs.writeFileSync(outputPath, result.css);
+    })
+
+    const processor = postcss([
+        tailwindcss(),
+    ]);
+    
     config.addShortcode("a", function (url, text) {
         return `<a class="ext-link" target="_blank" rel="noopener noreferrer"
                 href="${url}">${text}<span>&nearrow;</span></a>`;
     });
-    
+
     return {
         markdownTemplateEngine: 'njk',
         dataTemplateEngine: 'njk',
