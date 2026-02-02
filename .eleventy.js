ecd55f21fedbe92fb300319ca3cfc02fcefb8ad8
Whit Waltman <whitwaltman@mailbox.org>
2026-02-01 19:26:43 -0800

    inline css w/ nunjucks variable (h/t robin rendle)

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,14 @@
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
-import postcss from 'postcss';
-import tailwindcss from '@tailwindcss/postcss';
 import filters from './_utils/filters.js';
 import { eleventyImageTransformPlugin } from '@11ty/eleventy-img';
 
 export default async function (config) {
-    // Copy `img/` to `_site/img`
-    config.addPassthroughCopy('img');
+    // Copy public assets
+    config.addPassthroughCopy({
+        "./public": "/",
+    });
 
     // Add global default layout
     config.addGlobalData('layout', 'base.njk');
@@ -18,33 +18,9 @@ export default async function (config) {
         config.addFilter(filter, filters[filter]);
     });
 
-    // Before eleventy build, process css using postcss + tailwind
-    // Copy processor results to `_site/css/main.css`
-    config.on('eleventy.before', async () => {
-        const inputPath = path.resolve('./css/main.css');
-        const outputPath = path.resolve('./_site/css/main.css');
-        const cssContent = fs.readFileSync(inputPath, 'utf8');
-
-        const outputDir = path.dirname(outputPath);
-        if (!fs.existsSync(outputDir)) {
-            fs.mkdirSync(outputDir, { recursive: true });
-        }
-
-        const result = await processor.process(cssContent, {
-            from: inputPath,
-            to: outputPath,
-        });
-        fs.writeFileSync(outputPath, result.css);
-    });
-
-    // Create processor object
-    const processor = postcss([
-        tailwindcss(),
-    ]);
-
     // Add image transformer plugin with specified configuration
     config.addPlugin(eleventyImageTransformPlugin, {
-        urlPath: '/img',
+        urlPath: './img',
         outputDir: '.cache/@11ty/img/',
         failOnError: false,
         formats: ['webp'],
--- README.md
+++ README.md
@@ -7,3 +7,10 @@ I try my best to use tags to "track" non-trivial updates and version history. As
 - `major`: significant visual design and/or information architecture changes
 - `minor`: new pages are added/removed
 - `patch`: content updates to existing pages
+
+## notes to self
+
+- i removed `config.addPassthroughCopy('img');` from the config because it was unnecessarily copying the original image to the build directory
+    - now, the image transformer just takes the input url path and caches the output / copies it to the correct build location
+- in removing tailwind, i've decided to move my css files to `/public/css/`. although, now that i think about it, since i'm copying the whole 'public' folder over, maybe i don't need css in `/public` since i want to inline it...
+    - i'll revisit this decision later
