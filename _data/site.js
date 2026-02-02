a529e2ca591858a674d0d1ad60878e83d385a553
Whit Waltman <whitwaltman@mailbox.org>
2026-02-01 18:35:12 -0800

    add changelog file

--- _data/site.js
+++ _data/site.js
@@ -2,4 +2,5 @@ export default {
     title: "twelvefourseven.net",
     url: "https://twelvefourseven.net",
     latestBuild: new Date(),
+    repo: "https://github.com/whitwaltman/twelvefourseven.net"
 }
