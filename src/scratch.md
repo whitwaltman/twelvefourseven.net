e2d42db566d775ab5f85c99f34234c6d5277eae6
Whit Waltman <whitwaltman@mailbox.org>
2026-02-08 13:32:15 -0800

    updated time works

---
+++ src/scratch.md
@@ -0,0 +1,5 @@
+`ssh vps` -> twelvefourseven.net
+
+`ls -1 | grep "^[a-z]"` -> only lowercase directories
+
+`cd ~/org && ls -1 | grep -v "~"` -> exclude org backup files
--- src/src.11tydata.js
+++ src/src.11tydata.js
@@ -1,12 +1,24 @@
-// https://www.11ty.dev/docs/data-computed/
+import fs from "fs";
+
 export default {
     layout: "note.njk",
+    // https://www.11ty.dev/docs/data-computed/
     eleventyComputed: {
         permalink: (data) => `/${data.page.fileSlug}/`,
         title: (data) => {
             const slug = data.page.fileSlug.split("-").join(" ");
             return slug[0].toUpperCase() + slug.slice(1);
         },
-        
+        // https://joshtronic.com/2025/07/20/last-modified-date-eleventy/
+        updated: (data) => {
+            const stats = fs.statSync(data.page.inputPath);
+            const mtime = stats.mtime;
+
+            const ogDate = new Date(data.page.date).toISOString().split("T")[0];
+            const mDate = new Date(mtime).toISOString().split("T")[0];
+
+            if (mDate !== ogDate) return mtime;
+            return null;
+        }
     }
 }
