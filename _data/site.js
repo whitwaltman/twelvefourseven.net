f94cedcee758b79412277712ec73d55ae9ed98b8
Whit Waltman <whitwaltman@mailbox.org>
2026-02-21 16:57:08 -0800

    move stuff

--- _data/site.js
+++
@@ -1,7 +0,0 @@
-export default {
-	title: "twelvefourseven.net",
-	url: "https://twelvefourseven.net",
-	description: "The public notebook of Whit Waltman",
-	latestBuild: new Date(),
-	repo: "https://github.com/whitwaltman/twelvefourseven.net"
-};
