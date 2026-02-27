7af4c3a89b54214c12d0426601db43a9fd8a28f8
Whit Waltman <whitwaltman@mailbox.org>
2026-02-27 09:25:21 -0800

    add new content collection

---
+++ collections/favoriteAlbums.js
@@ -0,0 +1,98 @@
+export default [
+    {
+        title: "Lighght (Deluxe Version)",
+        artist: "Kishi Bashi",
+        release: 2014,
+        length: 47
+    },
+    {
+        title: "This Is Why",
+        artist: "Paramore",
+        release: 2023,
+        length: 36
+    },
+    {
+        title: "I Had The Blues But I Shook Them Loose",
+        artist: "Bombay Bicycle Club",
+        release: 2009,
+        length: 47
+    },
+    {
+        title: "In Rainbows",
+        artist: "Radiohead",
+        release: 2007,
+        length: 42
+    },
+    {
+        title: "Enter The Wu-Tang (36 Chambers) [Expanded Edition]",
+        artist: "Wu-Tang Clan",
+        release: 1993,
+        length: 70
+    },
+    {
+        title: "After Laughter",
+        artist: "Paramore",
+        release: 2017,
+        length: 42
+    },
+    {
+        title: "Sound of Silver",
+        artist: "LCD Soundsystem",
+        release: 2007,
+        length: 56
+    },
+    {
+        title: "Speaking in Tongues (Deluxe Version)",
+        artist: "Talking Heads",
+        release: 1983,
+        length: 58
+    },
+    {
+        title: "Revolver (Remastered)",
+        artist: "The Beatles",
+        release: 1966,
+        length: 34
+    },
+    {
+        title: "BRAT",
+        artist: "Charli xcx",
+        release: 2024,
+        length: 41
+    },
+    {
+        title: "Red, Yellow & Blue",
+        artist: "Born Ruffians",
+        release: 2008,
+        length: 39
+    },
+    {
+        title: "Everything Now",
+        artist: "Arcade Fire",
+        release: 2017,
+        length: 47
+    },
+    {
+        title: "Konk (Deluxe)",
+        artist: "The Kooks",
+        release: 2008,
+        length: 67
+    },
+    {
+        title: "What Do You Think About the Car?",
+        artist: "Declan McKenna",
+        release: 2017,
+        length: 47
+    },
+    {
+        title: "Blossom (Deluxe)",
+        artist: "Milky Chance",
+        release: 2017,
+        length: 81
+    },
+    {
+        title: "Vessel",
+        artist: "Twenty One Pilots",
+        release: 2013,
+        length: 47
+    }
+];
