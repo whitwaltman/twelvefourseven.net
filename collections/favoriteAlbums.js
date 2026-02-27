d3ee725bef33cc8a9a3c9a74db46251c9e85aae3
Whit Waltman <whitwaltman@mailbox.org>
2026-02-27 10:33:17 -0800

    Add content loader for albums collection

--- collections/favoriteAlbums.js
+++ collections/favoriteAlbums.js
@@ -1,98 +1,183 @@
 export default [
     {
+        id: "001",
         title: "Lighght (Deluxe Version)",
         artist: "Kishi Bashi",
         release: 2014,
         length: 47
     },
     {
+        id: "002",
         title: "This Is Why",
         artist: "Paramore",
         release: 2023,
         length: 36
     },
     {
+        id: "003",
         title: "I Had The Blues But I Shook Them Loose",
         artist: "Bombay Bicycle Club",
         release: 2009,
         length: 47
     },
     {
+        id: "004",
         title: "In Rainbows",
         artist: "Radiohead",
         release: 2007,
         length: 42
     },
     {
+        id: "005",
         title: "Enter The Wu-Tang (36 Chambers) [Expanded Edition]",
         artist: "Wu-Tang Clan",
         release: 1993,
         length: 70
     },
     {
+        id: "006",
         title: "After Laughter",
         artist: "Paramore",
         release: 2017,
         length: 42
     },
     {
+        id: "007",
         title: "Sound of Silver",
         artist: "LCD Soundsystem",
         release: 2007,
         length: 56
     },
     {
+        id: "008",
         title: "Speaking in Tongues (Deluxe Version)",
         artist: "Talking Heads",
         release: 1983,
         length: 58
     },
     {
+        id: "009",
         title: "Revolver (Remastered)",
         artist: "The Beatles",
         release: 1966,
         length: 34
     },
     {
+        id: "010",
         title: "BRAT",
         artist: "Charli xcx",
         release: 2024,
         length: 41
     },
     {
+        id: "011",
         title: "Red, Yellow & Blue",
         artist: "Born Ruffians",
         release: 2008,
         length: 39
     },
     {
+        id: "012",
         title: "Everything Now",
         artist: "Arcade Fire",
         release: 2017,
         length: 47
     },
     {
+        id: "013",
         title: "Konk (Deluxe)",
         artist: "The Kooks",
         release: 2008,
         length: 67
     },
     {
+        id: "014",
         title: "What Do You Think About the Car?",
         artist: "Declan McKenna",
         release: 2017,
         length: 47
     },
     {
-        title: "Blossom (Deluxe)",
-        artist: "Milky Chance",
-        release: 2017,
-        length: 81
-    },
-    {
+        id: "015",
         title: "Vessel",
         artist: "Twenty One Pilots",
         release: 2013,
         length: 47
+    },
+    {
+        id: "016",
+        title: "Paramore",
+        artist: "Paramore",
+        release: 2013,
+        length: 63
+    },
+    {
+        id: "017",
+        title: "Good Morning It's Now Tomorrow",
+        artist: "Matt Maltese",
+        release: 2021,
+        length: 44
+    },
+    {
+        id: "018",
+        title: "Petals for Armor",
+        artist: "Hayley Williams",
+        release: 2020,
+        length: 55
+    },
+    {
+        id: "019",
+        title: "American Idiot",
+        artist: "Green Day",
+        release: 2004,
+        length: 57
+    },
+    {
+        id: "020",
+        title: "The Fray",
+        artist: "The Fray",
+        release: 2009,
+        length: 42
+    },
+    {
+        id: "021",
+        title: "Virgin",
+        artist: "Lorde",
+        release: 2025,
+        length: 34
+    },
+    {
+        id: "022",
+        title: "Rubber Soul (Remastered 2009)",
+        artist: "The Beatles",
+        release: 1965,
+        length: 35
+    },
+    {
+        id: "023",
+        title: "Both Sides Of The Brain",
+        artist: "Del The Funky Homosapien",
+        release: 2000,
+        length: 73
+    },
+    {
+        id: "024",
+        title: "Aim and Ignite (Deluxe Version)",
+        artist: "fun.",
+        release: 2009,
+        length: 63
+    },
+    {
+        id: "025",
+        title: "Spooky Language",
+        artist: "The Palmer Squares",
+        release: 2012,
+        length: 24,
+        notes: [
+            "Depending on who you ask, Spooky Language could be classified as either an album or an EP. But Spotify categorizes it as the former, so let's go with that.",
+            "I first found The Palmer Squares in 2022. The title track quickly became one of my favorites, and I began playing it on loop.",
+            "I loved Spooky Language for its clever wordplay and wide-ranging cultural references, which is a throughline for the entire album.",
+            "It's an impressive debut effort, and remains my favorite release from The Palmer Squares."
+        ].join(" ")
     }
 ];
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,28 @@
 import { defineCollection } from 'astro:content';
-// glob loader creates entries from dirs, file loader creates multiple entries from a single file
 import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
+import favoriteAlbums from '../collections/favoriteAlbums';
+
+const albumSchema = z.object({
+    id: z.string(),
+    title: z.string(),
+    artist: z.string(),
+    release: z.number().int().positive().min(1900).max(2026),
+    length: z.number().int().positive(),
+    notes: z.string().optional(),
+});
+
+type Album = z.infer<typeof albumSchema>;
+
+const albums = defineCollection({
+    loader: async () => {
+        return favoriteAlbums.map((album: Album) => ({
+            id: album.id,
+            ...album
+        }));
+    },
+    schema: albumSchema
+});
 
 const garden = defineCollection({
     loader: glob({ pattern: ["**/*.md", "**/*.mdx"], base: "./garden" }),
@@ -14,4 +35,4 @@ const garden = defineCollection({
     })
 });
 
-export const collections = { garden };
+export const collections = { albums, garden };
